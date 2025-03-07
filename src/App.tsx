48959a759e7111bfa8ec6363e730408c2e762720
Artem-Brui <artembryj@gmail.com>
2025-03-07 11:48:19 +0100

    update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
   console.log('RENDER APP');
 
   return (
-    <BrowserRouter basename="/Books-Test">
+    <BrowserRouter basename="/">
       <Routes>
         <Route
           path="/"
